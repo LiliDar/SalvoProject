d2728888ece68aafbbbb80c3d0009285db7a721e
LiliDar <43753269+LiliDar@users.noreply.github.com>
2019-01-18 12:27:15 +0100

    Ships

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -12,16 +12,26 @@ var app = new Vue({
         userInfo: [],
         playerEmail: [],
         opponentEmail: [],
-        adjacentCells: [],
         currentPlayer: [],
 
+        adjacentCells: [],
         shipLocations: [],
+        secondClickId: [],
+        locationsArray: [],
 
         userShips: [],
         userSalvos: [],
         enemySalvos: [],
 
+        shipType: {
+            'carrier': 5,
+            'battelship': 4,
+            'submarine': 3,
+            'destroyer': 3,
+            'patrol': 2,
+        },
         ship: '',
+        shipClicks: 0,
         shipLogo: [],
 
         userGrid: "U",
@@ -67,8 +77,10 @@ var app = new Vue({
                     /*app.printSalvos(app.userSalvos, app.placeOpponentSalvo);
                     app.printSalvos(app.enemySalvos, app.placeUserSalvo);*/
                     app.printShips();
-                    app.shipLocation(app.userGrid);
-                    app.shipLocation(app.opponentGrid);
+
+                    
+
+                    app.shipLocation()
 
                 })
 
@@ -144,7 +156,7 @@ var app = new Vue({
                 .catch(e => console.log(e))
         },
 
-        placeShips(ship) {
+        placeShips() {
 
             fetch('/api/games/players/' + this.id + '/ships', {
                     credentials: 'include',
@@ -154,66 +166,162 @@ var app = new Vue({
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify([{
-                        type: ship,
-                        location: ['A1', 'A2', 'A2']
+                        type: app.ship,
+                        location: app.shipLocations
                     }]),
                 })
                 .then(r => r.json().then(e => console.log(e)))
                 .catch(e => console.log(e))
         },
 
+        letters(letter) {
+            if (letter == 'A') {
+                return letter = 1;
+            } else if (letter == 'B') {
+                return letter = 2;
+            } else if (letter == 'C') {
+                return letter = 3;
+            } else if (letter == 'D') {
+                return letter = 4;
+            } else if (letter == 'E') {
+                return letter = 5;
+            } else if (letter == 'F') {
+                return letter = 6;
+            } else if (letter == 'G') {
+                return letter = 7;
+            } else if (letter == 'H') {
+                return letter = 8;
+            } else if (letter == 'I') {
+                return letter = 9;
+            } else if (letter == 'J') {
+                return letter = 10;
+            } else {
+                console.log('error')
+            }
+        },
 
-        shipLocation(letter) {
+        numbers(number) {
+            app.letters(app.rows)
+            if (number == 1) {
+                return number = 'A';
+            } else if (number == 2) {
+                return number = 'B';
+            } else if (number == 3) {
+                return number = 'C';
+            } else if (number == 4) {
+                return number = 'D';
+            } else if (number == 5) {
+                return number = 'E';
+            } else if (number == 6) {
+                return number = 'F';
+            } else if (number == 7) {
+                return number = 'G';
+            } else if (number == 8) {
+                return number = 'H';
+            } else if (number == 9) {
+                return number = 'I';
+            } else if (number == 10) {
+                return number = 'J';
+            } else {
+                console.log('error')
+            }
+        },
+
+
+        shipLocation() {
 
             document.getElementById('userTable').addEventListener('click', (e) => {
-                console.log(e.target.id)
 
-                let location = e.target.id;
-                app.adjacentCells = [];
-                let gameId = e.target.id;
-                let rowHeader = gameId.charAt(1);
-                let columnHeader = gameId.charAt(2);
 
-                let shipLocation = letter + rowHeader + columnHeader;
-                let newNumber = parseInt(columnHeader);
+                let locationCell = e.target.id;
+                console.log(locationCell)
+                let rowHeader = locationCell.charAt(1);
+                let columnHeader = locationCell.charAt(2);
+                let adjacentRows = app.numbers(app.rows);
+                console.log(adjacentRows)
 
+                let shipLocation = "U" + rowHeader + columnHeader;
+                app.shipLocations.push(shipLocation);
+                console.log(app.shipLocations.toString())
+
+                let adjacentColumns = parseInt(columnHeader);
+                let arrayRight = [];
+                let arrayLeft = [];
+                let arrayDown = [];
+                let arrayUp = [];
+
+                for (let i = 1; i < 5; i++) {
+                    if ((adjacentColumns + i) < 11) {
+                        let cell = rowHeader + (adjacentColumns + i);
+                        arrayRight.push(cell)
+                    }
+                    if ((adjacentColumns - i) > 0) {
+                        let cell = rowHeader + (adjacentColumns - i);
+                        arrayLeft.push(cell)
+                    }
+                    if ((adjacentRows + i) < 11) {
+                        let row = letter(adjacentRows + i);
+                        let cell = row + adjacentColumns;
+                        arrayDown.push(cell)
+                    }
+                    if ((adjacentRows - i) > 0) {
+                        let row = letter(adjacentRows - i);
+                        let cell = row + adjacentColumns;
+                        arrayUp.push(cell)
+                    }
+                }
+                console.log(arrayUp, arrayDown)
+
+                if (arrayRight.length <= 3) {
+                    arrayRight.forEach(cell => {
+                        document.getElementById("U" + cell).classList.add("not-possible-cell")
+                    })
+
+                } else {
+                    arrayRight.forEach(cell => {
+                        document.getElementById("U" + cell).classList.add("possible-cell")
+                    })
+                }
+
+
+                if (arrayLeft.length <= 3) {
+                    arrayLeft.forEach(cell => {
+                        document.getElementById("U" + cell).classList.add("not-possible-cell")
+                    })
+                } else {
+                    arrayLeft.forEach(cell => {
+                        document.getElementById("U" + cell).classList.add("possible-cell")
+                    })
+                }
 
                 e.target.classList.add(this.ship);
-                
-                console.log(this.ship)
-
-
-                //if(ship == 'carrier') {
-                //   e.target.classList.add('carrier');
-                // }
-
-                /*$(e.target).click(function () {
-                        $("#carrier").clone().appendTo("#" + location);
-                    });*/
 
 
             });
         },
-        
-        selectTrump(){
+
+        selectTrump() {
             this.ship = 'carrier'
         },
-        
-        selectPutin(){
+
+        selectPutin() {
             this.ship = 'battleship'
         },
-        
-        selectMerkel(){
+
+        selectMerkel() {
             this.ship = 'submarine'
         },
-        
-        selectMay(){
+
+        selectMay() {
             this.ship = 'destroyer'
         },
-        
-        selectOrban(){
+
+        selectOrban() {
             this.ship = 'patrol'
         },
-        
+
+
+
+
     },
 })
